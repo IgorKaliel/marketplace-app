bd75354c9443c6d080b9c53cd9d69e233103c5e1
Igor Kaliel <igorkaliel@gmail.com>
2025-11-24 17:31:28 -0300

    Implement Zustand to persist in a LocalStorage with AsyncStorage

---
+++ src/shared/store/user-store.ts
@@ -0,0 +1,45 @@
+import { create } from "zustand"
+import { UserInterface } from "@/shared/interface/user"
+import { persist, createJSONStorage } from "zustand/middleware"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+
+interface setSessionParams {
+  user: UserInterface
+  token: string
+  refreshToken: string
+}
+
+interface updateTokensParams {
+  token: string
+  refreshToken: string
+}
+
+export interface UserStore {
+  user: UserInterface | null
+  token: string | null
+  refreshToken: string | null
+  setSession: (sessionData: setSessionParams) => void
+  logout: () => void
+  updateTokens: (updateTokensData: updateTokensParams) => void
+}
+
+export const useUserStore = create<UserStore>()(
+  persist(
+    (set) => ({
+      user: null,
+      token: null,
+      refreshToken: null,
+
+      logout: () => {
+        set({ user: null, token: null, refreshToken: null })
+      },
+      setSession: (sessionData) => {
+        set({ ...sessionData })
+      },
+      updateTokens: (updateTokensData) => {
+        set({ ...updateTokensData })
+      },
+    }),
+    { name: "marketplace-auth", storage: createJSONStorage(() => AsyncStorage) }
+  )
+)
