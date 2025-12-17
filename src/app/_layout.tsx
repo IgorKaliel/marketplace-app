b3b65df4d258bde5e19ec822de38afbe29b57b24
Igor Kaliel <igorkaliel@gmail.com>
2025-12-17 12:55:09 -0300

    upgrade routes and validate user login and redirect

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,4 +1,5 @@
 import { AppModal } from "@/shared/components/molecules/AppModal"
+import { useUserStore } from "@/shared/store/user-store"
 import "@/styles/global.css"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
@@ -9,6 +10,8 @@ import ToastManager from "toastify-react-native"
 const queryClient = new QueryClient()
 
 export default function RootLayout() {
+  const { token } = useUserStore()
+  console.log(`token: ${token}`)
   return (
     <QueryClientProvider client={queryClient}>
       <Stack
